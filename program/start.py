d74b3e034f65966e673b925ab8cbc16c0780166d
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-04-05 09:50:39 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -85,7 +85,7 @@ async def start_(c: Client, message: Message):
                 ],
             ]
         ),
-        disable_web_page_preview=false,
+        disable_web_page_preview=true,
     )
 
 
