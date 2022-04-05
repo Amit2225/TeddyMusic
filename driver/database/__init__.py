634b08f9e011e9541d8c777b401369396adaf4ae
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-04-05 09:32:48 +0530

    Update __init__.py

--- driver/database/__init__.py
+++ driver/database/__init__.py
@@ -1,4 +1,4 @@
 """ database copyright """
 
 # thanks to TheHamkerCat (https://github.com/TheHamkerCat) for his WilliamButcherBot database.
-# implemented by levina-lab (https://github.com/levina-lab)
+# implemented by levina-lab (https://github.com/SuruXmanager)
